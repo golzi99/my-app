cd96b91099a59f9ff4baf3a81dda4a6f6f185332
Pavel <golzi99@gmail.com>
2024-03-22 15:04:26 +0300

    22.03.2024 First prototype of site

---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+export function Header() {
+    return (<header className='header'>
+        <img src='https://www.creativefabrica.com/wp-content/uploads/2021/03/20/Mountain-logo-Design-Graphics-9785421-1-1-580x435.png'></img>
+    </header>);
+}
